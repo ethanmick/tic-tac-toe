f0f323621d8faa9a4daf23a3b56c4856036a660f
Ethan Mick <ethan@ethanmick.com>
2022-09-06 20:06:14 -0400

    Tic Tac Toe

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,245 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import { useState } from 'react'
+
+type Player = 'X' | 'O' | '-'
+
+type Tile = {
+  x: number
+  y: number
+  player?: Player
+}
+
+type State = {
+  board: Tile[][]
+  winner?: Player
+}
+
+type BoardTileProps = {
+  onClick: (t: Tile) => void
+  tile: Tile
+}
+
+const BoardTile = ({ tile, onClick }: BoardTileProps) => {
+  return (
+    <div
+      className="h-24 w-24 bg-neutral-100 rounded-lg cursor-pointer font-light text-4xl flex justify-center items-center"
+      onClick={() => onClick(tile)}
+    >
+      {tile.player || ''}
+    </div>
+  )
+}
+
+type GameOverProps = {
+  winner?: Player
+  onPlayAgain: () => void
+}
+
+const GameOver = ({ winner, onPlayAgain }: GameOverProps) => (
+  <div className="absolute inset-0 backdrop-blur-sm flex flex-col justify-center items-center">
+    <div className="text-4xl mb-2">
+      {winner === 'X' && <>You Win</>}
+      {winner === 'O' && <>You Lose</>}
+      {winner === '-' && <>Draw</>}
+    </div>
+    <div>
+      <button
+        className="rounded bg-sky-400 px-4 py-2 cursor-pointer hover:bg-sky-500"
+        onClick={onPlayAgain}
+      >
+        Play Again
+      </button>
+    </div>
+  </div>
+)
+
+const emptyBoard = [
+  [
+    { x: 0, y: 0 },
+    { x: 1, y: 0 },
+    { x: 2, y: 0 }
+  ],
+  [
+    { x: 0, y: 1 },
+    { x: 1, y: 1 },
+    { x: 2, y: 1 }
+  ],
+  [
+    { x: 0, y: 2 },
+    { x: 1, y: 2 },
+    { x: 2, y: 2 }
+  ]
+]
 
 const Home: NextPage = () => {
+  const [game, setGame] = useState<State>({
+    board: emptyBoard
+  })
+
+  const onPlayerMove = (t: Tile) => {
+    if (isOver(game)) {
+      return
+    }
+
+    const copy: State = JSON.parse(JSON.stringify(game))
+    copy.board[t.y][t.x].player = 'X'
+
+    let winner = isOver(copy)
+    if (winner) {
+      copy.winner = winner
+      setGame(copy)
+      return
+    }
+
+    ai(copy)
+
+    winner = isOver(copy)
+    if (winner) {
+      copy.winner = winner
+      setGame(copy)
+      return
+    }
+
+    setGame(copy)
+  }
+
+  const restart = () => {
+    setGame({
+      board: emptyBoard
+    })
+  }
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+    <div className="flex justify-center mt-12">
+      <div className="relative">
+        {game.board.map((row, key) => (
+          <div key={key} className="flex gap-4 mb-4">
+            {row.map((tile, key) => (
+              <BoardTile key={key} tile={tile} onClick={onPlayerMove} />
+            ))}
+          </div>
+        ))}
+        {game.winner && <GameOver winner={game.winner} onPlayAgain={restart} />}
+      </div>
     </div>
   )
 }
 
 export default Home
+
+function ai(game: State): State {
+  while (true) {
+    const [x, y] = [r(0, 2), r(0, 2)]
+    const tile = game.board[y][x]
+    if (hasPlayer(tile)) {
+      continue
+    }
+    tile.player = 'O'
+    break
+  }
+  return game
+}
+
+function isOver(game: State): Player | undefined {
+  const { board } = game
+
+  const toCheck = [
+    {
+      check: [
+        { x: 0, y: 0 },
+        { x: 1, y: 0 },
+        { x: 2, y: 0 }
+      ]
+    },
+    {
+      check: [
+        { x: 0, y: 1 },
+        { x: 1, y: 1 },
+        { x: 2, y: 1 }
+      ]
+    },
+    {
+      check: [
+        { x: 0, y: 2 },
+        { x: 1, y: 2 },
+        { x: 2, y: 2 }
+      ]
+    },
+    {
+      check: [
+        { x: 0, y: 0 },
+        { x: 0, y: 1 },
+        { x: 0, y: 2 }
+      ]
+    },
+    {
+      check: [
+        { x: 1, y: 0 },
+        { x: 1, y: 1 },
+        { x: 1, y: 2 }
+      ]
+    },
+    {
+      check: [
+        { x: 2, y: 0 },
+        { x: 2, y: 1 },
+        { x: 2, y: 2 }
+      ]
+    },
+    {
+      check: [
+        { x: 0, y: 0 },
+        { x: 1, y: 1 },
+        { x: 2, y: 2 }
+      ]
+    },
+    {
+      check: [
+        { x: 2, y: 0 },
+        { x: 1, y: 1 },
+        { x: 0, y: 2 }
+      ]
+    }
+  ]
+
+  for (const checker of toCheck) {
+    const tiles = []
+    for (const c of checker.check) {
+      tiles.push(board[c.y][c.x])
+    }
+    if (isSame(...tiles)) {
+      return tiles[0].player
+    }
+  }
+
+  for (const row of board) {
+    for (const tile of row) {
+      if (!hasPlayer(tile)) {
+        return undefined
+      }
+    }
+  }
+
+  return '-'
+}
+
+function hasPlayer(t: Tile) {
+  return !!t.player
+}
+
+function isSame(...tiles: Tile[]): boolean {
+  if (!hasPlayer(tiles[0])) {
+    return false
+  }
+  const p = tiles[0].player
+  for (const t of tiles) {
+    if (t.player != p) {
+      return false
+    }
+  }
+  return true
+}
+
+function r(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
